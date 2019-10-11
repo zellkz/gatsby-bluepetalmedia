5db09d8aacb1c5749a35bd696a1086e13437390a
Luke Hayes <zellkz@gmail.com>
2019-10-10 21:04:43 -0500

    logo!

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 import Footer from './Footer'
-import avatar from '../assets/images/bluepetal.png'
+import avatar from '../assets/images/bluepetalmedia-logo100.png'
 
 class Header extends React.Component {
     render() {
         return (
             <header id="header">
                 <div className="inner">
+                    <img src={avatar}  />
                     <h1><strong>Blue Petal Media</strong></h1>
                      We create stunning works of film and imagery using top of the line equipment from cameras to drones.
                 </div>
