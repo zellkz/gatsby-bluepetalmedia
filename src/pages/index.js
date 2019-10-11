fa2e067923bc0cc2080fba89145d8e8c96d6078e
Luke Hayes <zellkz@gmail.com>
2019-10-10 19:08:56 -0500

    button styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -79,12 +79,11 @@ class HomeIndex extends React.Component {
                                         <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
                                         <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
                                         <div className="12u"><textarea name="message" id="message" placeholder="Message" rows="4"></textarea></div>
-
                                     </div>
                                     <div className="row uniform 50%">
                                         <div className="12u">
                                             <ul className="actions">
-                                                <li><button type="submit">Send Message</button></li>
+                                                <li><input type="submit" value="Send Message" /></li>
                                             </ul>
                                         </div>
                                     </div>
